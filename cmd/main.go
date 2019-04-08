8d9193f0f432c9b3d23cb52dcb8a8feba9641a48
Will Daly <will.e.daly@gmail.com>
2019-04-08 06:49:14 -0700

    Default DB path

--- cmd/main.go
+++ cmd/main.go
@@ -4,19 +4,32 @@ import (
 	"fmt"
 	"github.com/wedaly/local-news/internal/app"
 	"os"
+	"os/user"
+	"path"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "Usage: %v DB_PATH\n", os.Args[0])
-		os.Exit(1)
+	config := app.Config{DBPath: getDefaultDBPath()}
+	if len(os.Args) > 1 {
+		config.DBPath = os.Args[1]
 	}
-
-	config := app.Config{DBPath: os.Args[1]}
 
 	err := app.Run(config)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unexpected error: %v\n", err)
 		os.Exit(1)
+	}
+}
+
+func getDefaultDBPath() string {
+	const dbName string = ".localnews.db"
+	if usr, err := user.Current(); err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Warning: could not find user's home directory: %v\n",
+			err)
+		return dbName
+    } else {
+		return path.Join(usr.HomeDir, dbName)
 	}
 }
